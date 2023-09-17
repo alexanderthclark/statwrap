2fa5424405e54e2c6cf1806283897c53d54e3826
Alexander Clark <alexanderthclark@gmail.com>
2023-09-17 16:45:20 -0400

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='StatWrap',
-    version='0.1.1',
+    version='0.1.3',
     description='A package for people new to statistics and Python.',
     long_description=long_description,  # This is the new line
     long_description_content_type="text/markdown",  # This is the new line
