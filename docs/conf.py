c8955e5e55d9f82a42df63e39a1c328079554d61
Alexander Clark <alexanderthclark@gmail.com>
2023-09-17 16:26:14 -0400

    docs v0

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here.
+sys.path.insert(0, os.path.abspath('../statwrap'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'StatWrap'
+copyright = '2023, Alexander Clark'
+author = 'Alexander Clark'
+release = '0.1.1'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',  # Generate docs from docstrings
+    'sphinx.ext.napoleon'  # Support for NumPy and Google style docstrings
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autodoc_mock_imports = ['IPython', 'pandas', 'numpy']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/modules.rst
@@ -0,0 +1,26 @@
+StatWrap Modules
+================
+
+fpp
+---
+
+.. automodule:: statwrap.fpp
+   :members:
+
+sheets
+------
+
+.. automodule:: statwrap.sheets
+   :members:
+
+core
+----
+
+.. automodule:: statwrap.core
+   :members:
+
+utils
+-----
+
+.. automodule:: statwrap.utils
+   :members:
