a1d8c0dd77be281b8e00f2b158a76d8ab69a5d7b
Alexander Clark <alexanderthclark@gmail.com>
2023-09-17 00:02:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 import setuptools
-setuptools.setup(name='StatWrap',
-version='0.1.0',
-description='A package for people new to statistics and Python.',
-url='https://github.com/alexanderthclark/statwrap',
-author='Alexander Clark',
-install_requires=['pandas','numpy','scipy', 'IPython'],
-author_email='',
-packages=setuptools.find_packages(),
-zip_safe=False)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='StatWrap',
+    version='0.1.0',
+    description='A package for people new to statistics and Python.',
+    long_description=long_description,  # This is the new line
+    long_description_content_type="text/markdown",  # This is the new line
+    url='https://github.com/alexanderthclark/statwrap',
+    author='Alexander Clark',
+    install_requires=['pandas','numpy','scipy', 'IPython'],
+    author_email='',  # consider adding your email
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
