996745bb5742a86d693e337c7ee1e74a97cae01d
Alexander Clark <alexanderthclark@users.noreply.github.com>
2024-01-30 12:29:44 -0500

    Update core.py

--- statwrap/core.py
+++ statwrap/core.py
@@ -8,57 +8,86 @@ from statwrap.sheets import sheets_setup
 
 @register_line_magic
 def use_fpp(line):
-	'''
-	Load the fpp module. 
+    '''
+    Load the fpp module.
 
-	This imports functions that adhere to the conventions found in "Statistics" by Freedman, Pisani, and Purves.
+    This imports functions that adhere to the conventions found in "Statistics" by Freedman, Pisani, and Purves.
 
 
-	Parameters
-	----------
-	line : str
-		Unused parameter retained for compatibility with IPython line magic.
+    Parameters
+    ----------
+    line : str
+        Unused parameter retained for compatibility with IPython line magic.
 
-	Returns
-	-------
-	None
+    Returns
+    -------
+    None
 
-	Examples
-	--------
-	Using this function in IPython:
+    Examples
+    --------
+    Using this function in IPython:
 
-	.. code-block:: python
+    .. code-block:: python
 
-		%use_fpp
-	'''
-	ip = get_ipython()
-	ip.ex("from statwrap.fpp import *")
-	fpp_setup()
+        %use_fpp
+    '''
+    ip = get_ipython()
+    ip.ex("from statwrap.fpp import *")
+    fpp_setup()
 
 @register_line_magic
 def use_sheets(line):
-	'''
-	Load the sheets module. 
+    '''
+    Load the sheets module.
 
-	This imports functions that adhere to the conventions specific to Google Sheets.
+    This imports functions that adhere to the conventions specific to Google Sheets.
 
-	Parameters
-	----------
-	line : str
-		Unused parameter retained for compatibility with IPython line magic.
+    Parameters
+    ----------
+    line : str
+        Unused parameter retained for compatibility with IPython line magic.
 
-	Returns
-	-------
-	None
+    Returns
+    -------
+    None
 
-	Examples
-	--------
-	Using this function in IPython:
+    Examples
+    --------
+    Using this function in IPython:
 
-	.. code-block:: python
+    .. code-block:: python
 
-		%use_sheets
-	'''
-	ip = get_ipython()
-	ip.ex("from statwrap.sheets import *")
-	sheets_setup()
+        %use_sheets
+    '''
+    ip = get_ipython()
+    ip.ex("from statwrap.sheets import *")
+    sheets_setup()
+
+@register_line_magic
+def use_all(line):
+    '''
+    Load the sheets and then fpp modules.
+
+    Parameters
+    ----------
+    line : str
+        Unused parameter retained for compatibility with IPython line magic.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    Using this function in IPython:
+
+    .. code-block:: python
+
+        %use_all
+    '''
+    ip = get_ipython()
+    ip.ex("from statwrap.sheets import *")
+    sheets_setup()
+    ip.ex("from statwrap.fpp import *")
+    fpp_setup()
+
