e3e1cfb32951cf95c702e99811a906e08cfa3bb7
Alexander Clark <alexanderthclark@users.noreply.github.com>
2026-09-21 00:03:44 -0400

    REL: Bump version to 0.3.2 (#71)

--- statwrap/__init__.py
+++ statwrap/__init__.py
@@ -8,7 +8,7 @@ except NameError:
 
 from .exceptions import StatwrapError, SimplePlotError
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 __all__ = ["use_fpp", "use_sheets", "use_all", "__version__", "StatwrapError", "SimplePlotError"]
 
